84f9fd1a6fd8516b588f1a1d51d8c4e4cb399db7
efe <e@gmail.com>
2023-05-26 00:34:44 +0300

    test

--- server.js
+++ server.js
@@ -20,5 +20,6 @@ app.get("/", (req, res) => res.send("API running"));
 
 app.use(notFound);
 app.use(errorHandler);
+console.log("deneme");
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
